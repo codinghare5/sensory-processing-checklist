55f9d8f951d3f5dece2d58929aa7ae11bd0506d6
elgud <eguest@zoho.com>
2021-08-03 18:40:02 +0100

    further re-jigging
    Made a new createCategoryArray. This just fetches the json file and return the resulting structure.
    
    Put the generic functions in a separate js file but have not called them.
    
    The result seems to work.

---
+++ js2/generic.js
@@ -0,0 +1,40 @@
+ // Generic Functions
+    ////////////////////////////////////////////////////////////////////////////////////////
+    // create array script copied from internet
+    // usage:
+    //      createArray() creates empty 1D array
+    //      createArray(2) creates 1D array of length 2
+    //      createArray(3, 2) creates 2D array, first dimension length 3, second dimension length 2
+    //      createArray(3,3,3,3) creates a 4D array where all dimensions have length 3
+    function createArray(length) {
+        var arr = new Array(length || 0),
+            i = length;
+
+        if (arguments.length > 1) {
+            var args = Array.prototype.slice.call(arguments, 1);
+            while(i--) arr[length-1 - i] = createArray.apply(this, args);
+        }
+
+        return arr;
+    }
+
+    // function to add the same event listener to every element in an array. 
+    Array.prototype.addEventListener = function(eventname, eventfunction) {
+        for (i=0 ; i<this.length ; i++)
+            this[i].addEventListener(eventname, eventfunction);
+    };
+
+    // create a function to console log every element in an array
+    Array.prototype.consolelog = function() {
+        for (i=0 ; i<this.length ; i++)
+            console.log(this[i]);
+    };
+
+    // convert Nodelists and HTLMCollections (and any other array like objects to an array.
+    function convertToArray(arrayLikeObject) {
+        var newArray = createArray(arrayLikeObject.length)
+        for (var i=0 ; i<newArray.length; i++)
+            newArray[i] = arrayLikeObject[i];
+
+        return newArray;
+    }
