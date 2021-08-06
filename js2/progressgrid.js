90c07e30784ba86f02af85d06958abb1241672c9
elgud <eguest@zoho.com>
2021-08-06 19:32:09 +0100

    not working yet, but saving quickly because keep loosing changes to progressgrid.njk

--- js2/progressgrid.js
+++ js2/progressgrid.js
@@ -1,50 +1,37 @@
-//function to set or remove the progress grid highlight:
+// class ProgressGrid
+// column is an html column of the progress grid
+// catindex is the category index
+// senseboxes is an array of the cells that make the up the column
+// eventListener is a function defined in the application. 
+//         It has one argument: the column that was clicked.
+class ProgressGrid {
+    constructor(column, index, eventListener) {
+        this.column = column;
+        this.catindex = index;
+        this.senseboxes = [...column.querySelectorAll(".progress-item")];
+        this.senseboxes.forEach(item => item.addEventListener('click', () => {
+                                        eventListener(item)}));
+    }
+
+    //function to set or remove the progress grid highlight:
     //  just set the color
     //      - LightGray for no highlight
     //      - Black for highlight
     // or change colours to suit
-    function setProgressGridHighlight(catindex, colour){
-        var item = progressGrid[+catindex].column;
-        item.style.borderColor = colour;
+    setProgressGridHighlight(catindex, colour){
+        this.column.style.borderColor = colour;
     }
+}
 
-    // Create progress grid column array
-    //  This is a list of the columns of the grid where each item contains
-    //      a progress-column
-    //      an array of progress-item
-    // An array of ProgressGrid is returned
-    function createProgressGrid() {
+// Create progress grid column array
+//  This is a list of the columns of the grid where each item contains
+//      a progress-column
+//      an array of progress-item
+// An array of ProgressGrid is returned
+function createProgressGrid() {
+    const pGrid = document.getElementById("progress-grid");
+    const progressColumnList = pGrid.querySelectorAll(".progress-column");
+    const progressArray = createArray(progressColumnList.length);
 
-        // (local) constructor for ProgressGrid
-        // column is a DOM object with class progress-column
-        class ProgressGrid {
-            constructor(column, catindex) {
-                this.column = column;
-
-                const items = column.querySelectorAll(".progress-item");
-                const itemsArray = createArray(items, length);
-                for (let i = 0; i < items.length; i++) {
-                    let item = items[i];
-                    itemsArray[+item.getAttribute("senseindex")] = item;
-                    item.addEventListener("click", function () {
-                        if (catindex != +currentCategoryIndex) {
-                            display(catindex, currentSenseIndex);
-                        }
-                    });
-                }
-
-                this.senseboxes = itemsArray;
-            }
-        }
-
-        const pGrid = document.getElementById("progress-grid");
-        const progressColumnList = pGrid.querySelectorAll(".progress-column");
-        const progressArray = createArray(progressColumnList.length);
-        for (j=0 ; j<progressArray.length ; j++){
-            item = progressColumnList[j];
-            catindex = item.getAttribute("catindex");
-            progressArray[+catindex] = new ProgressGrid(item, catindex);
-        }
-
-        return progressArray;
-    }
+    return progressArray.map((item, index) => new ProgressGrid(item,index,eventListender));
+}
