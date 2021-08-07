8b6d720c12cd43a2ab86c335410c01874c956902
elgud <eguest@zoho.com>
2021-08-07 21:38:16 +0100

    All seems to work now

--- js2/progressgrid.js
+++ js2/progressgrid.js
@@ -20,6 +20,49 @@ class ProgressGrid {
     setProgressGridHighlight(colour){
         this.column.style.borderColor = colour;
     }
+
+    clear(){
+        this.senseboxes.forEach(item => item.style.background = "White");   
+    }
+
+    // Function to colour in a cell of the progress grid
+    //  colours a cell according to senseindex
+    //  if value==0, the colour is whitesmoke irrespective of the sense
+    //  if value==1, the colour is HoneyDew
+    //  Otherwise colours are according to sense:
+    //      0   vision          Red
+    //      1   hearing         orange
+    //      2   touch           Yellow
+    //      3   smell           Green
+    //      4   taste           blue
+    //      5   proprioception  Indigo
+    //      6   balance         violet
+    // Change all these colours here so they are all in one place.
+    colourCell(senseindex, value) {
+        let box = this.senseboxes[senseindex];
+        if (+value == 0)
+            box.style.background = "WhiteSmoke";
+        else if (+value == 1)
+            box.style.background = "AntiqueWhite"; 
+        else {
+            switch(+senseindex) {
+                case 0: box.style.background = "Red";
+                        break;
+                case 1: box.style.background = "Orange";
+                        break;
+                case 2: box.style.background = "Yellow";
+                        break;
+                case 3: box.style.background = "Green";
+                        break;
+                case 4: box.style.background = "Blue";
+                        break;
+                case 5: box.style.background = "Indigo";
+                        break;
+                case 6: box.style.background = "Violet";
+                        break;                
+            }
+        }
+    }
 }
 
 // Create progress grid column array
